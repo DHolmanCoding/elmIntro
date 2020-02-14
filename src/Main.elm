dc99fca45d1dd9c4a8b4d432361f1d40f8f1bf2a
douglas <holmando@oregonstate.edu>
2020-02-13 18:53:17 -0800

    mocking a slack page example :-)

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,137 @@
 module Main exposing (main)
 
-import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (..)
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
 
-main =
-  Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
+type alias Message =
+    { author : String, time : String, text : String }
 
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
 
-    Decrement ->
-      model - 1
+sampleChannels : List String
+sampleChannels =
+    [ "software"
+    , "spam"
+    , "general"
+    , "data-science"
+    , "dev-ops"
+    , "general"]
 
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+
+sampleActiveChannel : String
+sampleActiveChannel =
+    "data-science"
+
+
+sampleMessages : List Message
+sampleMessages =
+    [ { author = "douglas", time = "9:00AM", text = "Hello, World! :-)" },
+      { author = "douglas", time = "10:00AM", text = "Testing out this starter code" }
     ]
+
+
+channelPanel : List String -> String -> Element msg
+channelPanel channels activeChannel =
+    let
+        activeChannelAttrs =
+            [ Background.color <| rgb255 117 179 201, Font.bold ]
+
+        channelAttrs =
+            [ paddingXY 15 5, width fill ]
+
+        channelEl channel =
+            el
+                (if channel == activeChannel then
+                    activeChannelAttrs ++ channelAttrs
+
+                 else
+                    channelAttrs
+                )
+            <|
+                text ("# " ++ channel)
+    in
+    column
+        [ height fill
+        , width <| fillPortion 1
+        , paddingXY 0 10
+        , scrollbars
+        , Background.color <| rgb255 92 99 118
+        , Font.color <| rgb255 255 255 255
+        ]
+    <|
+        List.map channelEl channels
+
+
+chatPanel : String -> List Message -> Element msg
+chatPanel channel messages =
+    let
+        header =
+            row
+                [ width fill
+                , paddingXY 20 5
+                , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
+                , Border.color <| rgb255 200 200 200
+                ]
+                [ el [] <| text ("#" ++ channel)
+                , Input.button
+                    [ padding 5
+                    , alignRight
+                    , Border.width 1
+                    , Border.rounded 3
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    { onPress = Nothing
+                    , label = text "Search"
+                    }
+                ]
+
+        messageEntry message =
+            column [ width fill, spacingXY 0 5 ]
+                [ row [ spacingXY 10 0 ]
+                    [ el [ Font.bold ] <| text message.author, text message.time ]
+                , paragraph [] [ text message.text ]
+                ]
+
+        messagePanel =
+            column [ padding 10, spacingXY 0 20, scrollbarY ] <|
+                List.map messageEntry messages
+
+        footer =
+            el [ alignBottom, padding 20, width fill ] <|
+                row
+                    [ spacingXY 2 0
+                    , width fill
+                    , Border.width 2
+                    , Border.rounded 4
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    [ el
+                        [ padding 5
+                        , Border.widthEach { right = 2, left = 0, top = 0, bottom = 0 }
+                        , Border.color <| rgb255 200 200 200
+                        , mouseOver [ Background.color <| rgb255 86 182 139 ]
+                        ]
+                      <|
+                        text "+"
+                    , el [ Background.color <| rgb255 255 255 255 ] none
+                    ]
+    in
+    column [ height fill, width <| fillPortion 5 ]
+        [ header
+        , messagePanel
+        , footer
+        ]
+
+
+main : Html msg
+main =
+    layout [ height fill ] <|
+        row [ height fill, width fill ]
+            [ channelPanel sampleChannels sampleActiveChannel
+            , chatPanel sampleActiveChannel sampleMessages
+            ]
